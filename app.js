dc971b8ba73902b4915f5bfd9da33b71fe807116
AddisonNoxy <34310754+AddisonNoxy@users.noreply.github.com>
2018-02-06 23:57:12 -0600

    Remove Bobby, add Clem

--- app.js
+++ app.js
@@ -157,46 +157,46 @@ client.on('message', message => {
     message.channel.sendMessage(mikhailSelect);
   }
   
-    //messages asking for bobby's friendster lines
-  if (message.content.startsWith(chat + 'bobby')) {
-    var bobbyArray = [
-      "Who cares about shooting? Let's see how good you are with your fists Hoofburger.\nOn 6/15/2003",
-      "Benny, dude, I thought I told you, not every time, alright? Wait for the signal.\nOn 6/15/2003"
-      "Ouch. Kicked when she's down. Ruthless... I'm proud of you my young Paduan learner.\nOn 6/07/2003",
-      "Hey, dude. Much like Dr. Love here, I too, am concerned with your unfocused rage and violent energies. You need to channel that force into something more positive. Like say, being in my gang. Benny is efficient for hurting people's feelings and being annoying, but I really need in my corner is pure, dumb muscle.\nOn 5/21/2003",
-      "And look at Benny here. He's small and some day one of these kids is going to TK his head off and there I'll there I'll be without an flunkie! So what do you say? Pay is 3% of all lunch money received, and all the smackdown you want to dish out.\nOn 6/22/2003",
-      "I pay by the pound. And speaking of pounds, maybe you better shut up unless you want some right now.\nOn 6/22/2003",
-      "Oh, I'll route something through you--my fist! hahaha!\nOn 6/22/2003",
-      "Shut up, Benny.\nOn 6/22/2003",
-      "Oh, good, because there's one in your foot locker right now, Boole. Ha ha ha! And I'm not going to tell you what it is!\nOn 6/11/2003",
-      "No, you stupid idiot. It's a surprise.\nOn 6/13/2003",
-      "Benny, seriously. You just earned yourself a four-limb indian burn.\nOn 6/15/2003",
-      "What a chicken. I could take that kid easy. First step is staple that damn hat directly onto his skull. Second step--ass whooping. Third step, chicks.\nOn 6/15/2003",
-      "New rule Bulgakov. Anybody so much as LOOKS at Chloe, they're psychic road kill. Got it?\nOn 6/19/2003",
-      "Sorry. I've been asked to rephrase that. Let's all remember to show each other respect and not say mean things to each other.\nOn 6/20/2003",
-      "Everybody should have a Benny. He takes his beatings like a man, and if you drop your change down the outhouse, he'll run down in there to get it for you, without too much resistance.\nOn 5/30/2003",
-      "Oh, hey guys. Bobby Zilch here. Sorry about that friends' list thing. You know, I keep trying to add you guys, but my computer keeps saying, \"REJECTED: These jocks are lame and they suck. Cannot accept. Beep, beep beep!\" I don't know what it is. It must be some sort of computer error.\nOn 6/19/2003",
-      "HA HA HA HA! Gottcha, Snagrash!\nOn 6/19/2003",
-      "Benny, what's this I hear about you playing a dirty trick on Chloe? I told you, everybody's fair game but her. So lay off. Don't ask why unless you want me to give you the five good reasons I have on my right hand, which I will curl up into a fist with which to hit upon your ass. Understand rubber band? I'm the boss, applesauce.\nOn 6/19/2003",
-      "Okay, I don't know what all those PBS words mean, but I'm going to assume you got my point, RE: my bossness. We clear, mr. Brains-a-Lot?\nOn 6/22/2003",
-      "That is the circle of life, Simba.\nOn 6/14/2003",
-      "So girls like him. Big deal. I got plenty of girls after me, and I don't have to pretend to like them to get 'em either.\nOn 6/17/2003",
-      "God I hate you two so much, I can feel it in my liver. Seriously, it's producing extra bile every time I hear your voice. You're killing me.\nOn 6/19/2003",
-      "P.S. That reminds me--Hedgemouth: Bobby and I want to be in the band.\nOn 6/25/2003",
-      "I'm going to be taps, at your funeral, if you don't let us in your band. heh.\nOn 6/27/2003",
-      "Or even better: team Take-a-Poo-on-You!\nOn 5/29/2003",
-      "Give it up, lame-os. I'm not putting you on my friends list. You suck.\nOn 5/29/2003",
-      "Tough titty said the kitty when the milk went dry.\nOn 5/29/2003",
-      "Hey new kid. Welcome to camp. You're dead meat.\nOn 5/09/2003",
-      "Nah. I'm outta here. Benny, he's all yours.\nOn 6/10/2003",
-      "Heh heh heh.\nSweetwind.\nHeh heh.\nOn 6/01/2003",
-      "Excellent. Now say something about their talent, quick, while that one is still stinging.\nOn 6/13/2003",
-      "Not bad, not bad. Now wrap up with a cheap shot about about their appearance.\nOn 6/15/2003",
-      "hm... solid. Weak ending, but all in all, a good effort.\nOn 6/16/2003"
+  //messages asking for clem's campster lines
+  if (message.content.startsWith(chat + 'clem')) {
+    var clemArray = [
+      'Speck of dust?\nOn 6/17/2003',
+      'From a mile away!\nOn 6/17/2003',
+      'Ants suck! J.T. Rocks!\nOn 6/17/2003',
+      "Yeah, it will be awesome!!!!!!!! If we get enough cheers together, maybe someone will start a sports team for us to cheer on.\nOn 6/9/2003",
+      "Ah Crystal, I guess it's just not going to work. Doesn't seem like anyone here's interested in real sports--it's all mind, mind, mind!\nBut that's okay. How would you ever appreciate the sweet taste of victory if we didn't also know the constant, comforting tonic of failure?\nOn 6/17/2003",
+      "That goes ditto for me!!! Double Ditto!!! OMFG!\nOn 5/21/2003",
+      "Not half as good as you, Crystal! Let's go talk to Coach Oleander right now! I'll bet he'll flip over the idea! :D! Maybe even literally! :D! :D!\nOn 5/19/2003",
+      "Yeah, like if we were... like killed in some freak accident or something.\nOn 6/17/2003",
+      "Omg! Elton is so good at canoeing!\nOn 5/24/2003",
+      "Total omg! Elton is like the MASTER of the telekinetic canoe!\nOn 5/24/2003",
+      "He's so much better than me!\nOn 5/24/2003",
+      "No, I'm terrible at canoeing. My dad says I'm a total idiot when it comes to canoeing. And most other things. He said he'd rather ride a seal into a pool of sharks than ride in a canoe into a lake with me. Sometimes I wonder if he's right--if I am to stupid for this world.\nOn 5/24/2003",
+      "Totally. But I guess Elton's pretty stoked.\nOn 5/24/2003",
+      "And his picture perfect childhood full of love and support! Yeah, Elton!\nOn 5/24/2003",
+      "I wish I were special like Dogen! And I had a hat that could kill people!\on 6/18/2003",
+      "But I'd only use it for good!\nOn 6/18/2003",
+      "Crystal, I think he means hurting other people.\nOn 6/13/2003",
+      "Hey hey hey there! That's no way to talk! We can still have a club! Just you and me! My brain hurts too! I want to die too! Sometimes. But then again sometimes I feel GGGGGGGRRRRREEEEEAAAT!!!\nOn 6/13/2003",
+      "Your shirt!\nOn 6/19/2003",
+      "Yeah, where did you get it? Mervyn's? Man they have the best shirts there. I gotta get my mom to take me to Mervyn's some time.\nWooo! Go Benny!\nOh, so anyhew... you're friends with Bobby, right? You know, he is the only person at camp who's not on our friends list. We're friends with EVERYONE else in camp. Super good friends in fact!\nOn 6/19/2003",
+      "Don't listen to him Crystal. He's just making fun of us!\nOn 6/19/2003",
+      "Crystal, there is no true way to be best friends forever in this finite universe. There is no 'forever' here. Only the world that lies beyond this one is vast enough to contain a best-friendship as infinite as ours. Maybe it is time we went there.\nOn 6/19/2003",
+      "You put the YOU in Unique! You and Quentin are both winners who are decidedly different, positively popular, and outrageously outrageous!!!!\nOn 6/18/2003",
+      "Yeah! Total team! With his canoeing skills and your super invisibility, together you could be like... like... Team Peek-a-boo Canoe!\nOn 5/29/2003",
+      "Ha! That's great too! Bobby, where do you get your ideas?\nOn 5/29/2003",
+      ">:| !!!\nOn 5/29/2003",
+      "This girl is the best! What's not to like? Crystal is so cool, I don't even know why she hangs out with a piece of dirt like me. Here's some love for ya girl: <3<3<3<3!!!\nOn 5/14/2003",
+      "Hey, Crystal! I just wanted you to know that I called a professional geologist and he agrees: you rock!\nGet it?\nOn 5/26/2003",
+      "Hey spontaneous group activities are great!\nOn 6/14/2003",
+      "OMG we have so much fun at camp! This is the best year ever! I could die now and be totally happy! I'm just saying it wouldn't be a complete tragedy if we died this year, because then we would have ended on a high note!\nOn 6/19/2003",
+      "Yeah, Kitty's just more unique on the outside, where we can see it. Some of us are more unique on the inside. In our hearts!\nSome of our hearts are big, and some are small, and some of them are dark, and still, and stopped beating long ago, and yet somehow we manage to slog through this shadow puppet theater day in and day out, waiting for the merciful velvet curtain of death.\nOn 6/11/2003",
+      "Three cheers for Maloof.\nHip hip hurray!\nHip hip hurray!\nHip hip-- oh forget it. He is kind of a baby.\nOn 5/23/2003",
+      "Where was I? Oh yeah...\nHip hip, hurray!\nOn 6/20/2003",
     ];
-    var bobbySelect = bobbyArray[Math.floor(Math.random()*bobbyArray.length)];
-    message.channel.sendMessage('Bobby Zilch said:');
-    message.channel.sendMessage(bobbySelect);
+    var clemSelect = clemArray[Math.floor(Math.random()*clemArray.length)];
+    message.channel.sendMessage('Clem Foote said:');
+    message.channel.sendMessage(clemSelect);
   }
   
     //messages asking for dogen's friendster lines
