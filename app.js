f959ba8890d6599e9badd1fd154214f3c9e8a6d4
AddisonNoxy <34310754+AddisonNoxy@users.noreply.github.com>
2018-02-07 00:05:45 -0600

    Remove Clem

--- app.js
+++ app.js
@@ -192,48 +192,6 @@ client.on('message', message => {
     message.channel.sendMessage(elkaSelect);
   }
   
-  //messages asking for clem's campster lines
-  if (message.content.startsWith(chat + 'clem')) {
-    var clemArray = [
-      'Speck of dust?\nOn 6/17/2003',
-      'From a mile away!\nOn 6/17/2003',
-      'Ants suck! J.T. Rocks!\nOn 6/17/2003',
-      "Yeah, it will be awesome!!!!!!!! If we get enough cheers together, maybe someone will start a sports team for us to cheer on.\nOn 6/9/2003",
-      "Ah Crystal, I guess it's just not going to work. Doesn't seem like anyone here's interested in real sports--it's all mind, mind, mind!\nBut that's okay. How would you ever appreciate the sweet taste of victory if we didn't also know the constant, comforting tonic of failure?\nOn 6/17/2003",
-      "That goes ditto for me!!! Double Ditto!!! OMFG!\nOn 5/21/2003",
-      "Not half as good as you, Crystal! Let's go talk to Coach Oleander right now! I'll bet he'll flip over the idea! :D! Maybe even literally! :D! :D!\nOn 5/19/2003",
-      "Yeah, like if we were... like killed in some freak accident or something.\nOn 6/17/2003",
-      "Omg! Elton is so good at canoeing!\nOn 5/24/2003",
-      "Total omg! Elton is like the MASTER of the telekinetic canoe!\nOn 5/24/2003",
-      "He's so much better than me!\nOn 5/24/2003",
-      "No, I'm terrible at canoeing. My dad says I'm a total idiot when it comes to canoeing. And most other things. He said he'd rather ride a seal into a pool of sharks than ride in a canoe into a lake with me. Sometimes I wonder if he's right--if I am to stupid for this world.\nOn 5/24/2003",
-      "Totally. But I guess Elton's pretty stoked.\nOn 5/24/2003",
-      "And his picture perfect childhood full of love and support! Yeah, Elton!\nOn 5/24/2003",
-      "I wish I were special like Dogen! And I had a hat that could kill people!\on 6/18/2003",
-      "But I'd only use it for good!\nOn 6/18/2003",
-      "Crystal, I think he means hurting other people.\nOn 6/13/2003",
-      "Hey hey hey there! That's no way to talk! We can still have a club! Just you and me! My brain hurts too! I want to die too! Sometimes. But then again sometimes I feel GGGGGGGRRRRREEEEEAAAT!!!\nOn 6/13/2003",
-      "Your shirt!\nOn 6/19/2003",
-      "Yeah, where did you get it? Mervyn's? Man they have the best shirts there. I gotta get my mom to take me to Mervyn's some time.\nWooo! Go Benny!\nOh, so anyhew... you're friends with Bobby, right? You know, he is the only person at camp who's not on our friends list. We're friends with EVERYONE else in camp. Super good friends in fact!\nOn 6/19/2003",
-      "Don't listen to him Crystal. He's just making fun of us!\nOn 6/19/2003",
-      "Crystal, there is no true way to be best friends forever in this finite universe. There is no 'forever' here. Only the world that lies beyond this one is vast enough to contain a best-friendship as infinite as ours. Maybe it is time we went there.\nOn 6/19/2003",
-      "You put the YOU in Unique! You and Quentin are both winners who are decidedly different, positively popular, and outrageously outrageous!!!!\nOn 6/18/2003",
-      "Yeah! Total team! With his canoeing skills and your super invisibility, together you could be like... like... Team Peek-a-boo Canoe!\nOn 5/29/2003",
-      "Ha! That's great too! Bobby, where do you get your ideas?\nOn 5/29/2003",
-      ">:| !!!\nOn 5/29/2003",
-      "This girl is the best! What's not to like? Crystal is so cool, I don't even know why she hangs out with a piece of dirt like me. Here's some love for ya girl: <3<3<3<3!!!\nOn 5/14/2003",
-      "Hey, Crystal! I just wanted you to know that I called a professional geologist and he agrees: you rock!\nGet it?\nOn 5/26/2003",
-      "Hey spontaneous group activities are great!\nOn 6/14/2003",
-      "OMG we have so much fun at camp! This is the best year ever! I could die now and be totally happy! I'm just saying it wouldn't be a complete tragedy if we died this year, because then we would have ended on a high note!\nOn 6/19/2003",
-      "Yeah, Kitty's just more unique on the outside, where we can see it. Some of us are more unique on the inside. In our hearts!\nSome of our hearts are big, and some are small, and some of them are dark, and still, and stopped beating long ago, and yet somehow we manage to slog through this shadow puppet theater day in and day out, waiting for the merciful velvet curtain of death.\nOn 6/11/2003",
-      "Three cheers for Maloof.\nHip hip hurray!\nHip hip hurray!\nHip hip-- oh forget it. He is kind of a baby.\nOn 5/23/2003",
-      "Where was I? Oh yeah...\nHip hip, hurray!\nOn 6/20/2003",
-    ];
-    var clemSelect = clemArray[Math.floor(Math.random()*clemArray.length)];
-    message.channel.sendMessage('Clem Foote said:');
-    message.channel.sendMessage(clemSelect);
-  }
-  
     //messages asking for dogen's friendster lines
   if (message.content.startsWith(chat + 'dogen')) {
     var dogenArray = [
